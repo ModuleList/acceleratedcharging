ff68a58a15ca34a819a1728d036a33ae66371e56
heinu123 <heinu333@gmail.com>
2023-03-20 21:22:10 +0800

    修复编译脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-
+#!/system/bin/sh
 if ! command -v go >/dev/null 2>&1; then
   echo "未安装golang"
   exit
@@ -7,6 +7,14 @@ if ! command -v zip >/dev/null 2>&1; then
   echo "未安装zip"
   exit
 fi
-
-go build -ldflags="-s -w" -o ./module/system/bin/charge-current ./module/system/bin/charge-current.go
-zip -q -r acceleratedcharging.zip ./module/*
+cd module
+go build -ldflags="-s -w" -o ./system/bin/charge-current ./system/bin/charge-current.go
+if [ -f ./system/bin/charge-current ];then
+    rm -rf ./system/bin/charge-current.go
+else
+    echo "编译失败"
+    exit
+fi
+zip -r acceleratedcharging.zip *
+echo "打包编译成功"
+echo "模块在module文件夹下"
